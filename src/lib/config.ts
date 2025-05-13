617c1ab1fbf71abdc73464f83d6ae068cd85ef24
souravdas <dass7550@gmail.com>
2025-05-13 11:12:07 +0600

    email workflow

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,10 +9,10 @@ const config = {
     },
 
     upstash: {
-      redisUrl: process.env.UPSTASH_REDIS_REST_URL,
-      redisToken: process.env.UPSTASH_REDIS_REST_TOKEN,
-      qstashUrl: process.env.QSTASH_URL,
-      qstashToken: process.env.QSTASH_TOKEN,
+      redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
+      redisToken: process.env.UPSTASH_REDIS_REST_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToken: process.env.QSTASH_TOKEN!,
     },
   },
 };
